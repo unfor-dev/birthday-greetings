f89c35fdaf8ffebbf325deac7d92276416fd7073
unfor-dev <hamzayevasadulloh2005@gmail.com>
2025-08-05 16:21:29 +0500

    Adding project starter

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+import App from './App'
+import { Leva } from 'leva'
+
+createRoot(document.getElementById('root')).render(
+  <>
+  </>
+)
